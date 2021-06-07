23a11721faa1f08ee033f477efad93bc2e8a0b3e
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2021-06-07 11:31:52 -0500

    vue config update to deploy

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,8 @@ module.exports = {
             args[0].title = 'Mini Netflix'
             return args
         })
-    }
+    },
+    baseUrl: process.env.NODE_ENV === 'production'
+    ? '/mini-netflix/'
+    : '/'
 }
